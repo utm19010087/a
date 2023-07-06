f9850445bbf6ea118fafa506d707845a765b4dc4
Cisco Huerta <utm19010087@utma.edu.mx>
2023-07-06 10:35:44 -0600

    s

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -10,42 +10,50 @@ export class PlacesService {
     new Place
     (
       "p1",
-      "Aculco, Estado de México",
-      "Este Pueblo Mágico reside entre montañas, peñas y cascadas, donde el sol inunda de luz calles, callejones y refleja su luz en sus blancas paredes",
-      "https://www.gob.mx/cms/uploads/image/file/530648/Mexico_Aculco_Cascada-Tixhin_u-web.jpg",
-      350
+      "Coscomatepec",
+      "Sin duda te encantará llegar a este lugar entre montañas con la característica neblina ¡y con un rico olor a café! ",
+      "https://www.mexicodestinos.com/blog/wp-content/uploads/2017/06/Coscomatepec.png",
+      4250
     ),
     new Place
     (
       "p2",
-      "Ajijic, Jalisco",
-      "Fundada en 1531, su nombre proviene del náhuatl Axixic que significa 'donde se derrama el agua' o 'donde brota el agua', es una de las villas más antiguas de México.",
+      "Orizaba",
+      "ciudad de las aguas alegres",
       "https://www.gob.mx/cms/uploads/article/main_image/102841/Copia_de_ajijic-jalisco-dane-strom-2601_2.jpg",
-      390
+      4290
     ),
     new Place
     (
       "p3",
-      "Álamos, Sonora",
-      "Muy cerca de la frontera con Sinaloa y Chihuahua se encuentra Álamos, un Pueblo Mágico que sorprende por su arquitectura y tradiciones",
+      "Zozocolco de Hidalgo",
+      "entre los cerros de la Golondrina, Pelón y cerro Buena Vista",
       "https://www.gob.mx/cms/uploads/article/main_image/84635/A_lamos-Parroquia-de-La-Puri_sima-Concepcio_n-web.jpg",
-      410
+      4410
     ),
     new Place
     (
       "p4",
-      "Amealco de Bonfil, Querétaro",
-      " Es un lugar especial por sus cuerpos de agua, y sus paisajes boscosos",
-      "https://www.gob.mx/cms/uploads/image/file/531307/Amealco-Quere_taro-Cerro-de-la-Cruz-web.jpg",
-      480
+      "Papantla",
+      "Casa de los Voladores de Papantla",
+      "https://www.mexicodestinos.com/blog/wp-content/uploads/2017/06/Voladores_de_Papantla.png",
+      4480
     ),
     new Place
     (
       "p5",
-      "Aquismón, San Luis Potosí",
-      "El Pueblo Mágico de Aquismón es la Meca para los amantes de los deportes de aventura como: abseiling, climbing, hiking, kayaking, mountain biking, parachiting rafting, o wingsuit flying",
-      "https://www.gob.mx/cms/uploads/article/main_image/85296/Aquismon-Cascada-Tamul-web.jpg",
-      580
+      "Coatepec",
+      "Parque Hidalgo",
+      "https://www.mexicodestinos.com/blog/wp-content/uploads/2017/06/Parroquia_San_Jeronimo_Coatepec.jpg",
+      4580
+    ),
+    new Place
+    (
+      "p6",
+      "Xico",
+      "es un bello pueblo mágico que te embruja por su cálido ambiente",
+      "https://www.mexicodestinos.com/blog/wp-content/uploads/2017/06/Iglesia_de_Xico.png",
+      4580
     )
   ];
 
